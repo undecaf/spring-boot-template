7d41c742288dd0af68b08c3f4a8541da1f23684e
Ferdinand Kasper <ferdinand.kasper@gmx.at>
2019-05-24 11:49:06 +0200

    Server: Persistent-Hilfsmethoden optimiert für den Fall, dass keine Veränderung erfolgt.

--- src/main/java/server/models/Persistent.java
+++ src/main/java/server/models/Persistent.java
@@ -41,7 +41,7 @@ public class Persistent {
      *
      * @param newCollection
      *      neu zuzuweisende {@link Collection} von Entities der n-Seite
-     * @param setOne
+     * @param setEntity
      *      set-{@linkplain BiConsumer Methode} der n-Seite für die Entity der 1-Seite
      * @param getCollection
      *      get-{@linkplain Function Methode} der 1-Seite für die {@link Collection} von Entities der n-Seite
@@ -55,21 +55,24 @@ public class Persistent {
      *      neu zugewiesene {@link Collection} von Entities der n-Seite
      */
     protected <O extends Persistent, M extends Persistent, C extends Collection<M>>
-            C setOneToMany(C newCollection, BiConsumer<M, O> setOne, Function<O, C> getCollection) {
+            C setOneToMany(C newCollection, BiConsumer<M, O> setEntity, Function<O, C> getCollection) {
 
-        C collection;
+        C collection = getCollection.apply((O) this);
 
-        // Gibt es derzeit eine Collection von n-Entities?
-        if ((collection = getCollection.apply((O) this)) != null) {
-            // Alle bisherigen n-Entities entfernen
-            // Um ConcurrentModificationException zu verhindern:
-            new ArrayList<M>(collection).forEach(e -> setOne.accept(e, null));
-        }
+        // Soll überhaupt eine andere Collection zugewiesen werden?
+        if (newCollection != collection) {
+            // Gibt es derzeit eine Collection von n-Entities?
+            if (collection != null) {
+                // Alle bisherigen n-Entities entfernen
+                // Um ConcurrentModificationException zu verhindern:
+                new ArrayList<M>(collection).forEach(e -> setEntity.accept(e, null));
+            }
 
-        // Wurde eine neue Collection von n-Entities angegeben?
-        if (newCollection != null) {
-            // Alle neuen n-Entities hinzufügen
-            newCollection.forEach(e -> setOne.accept(e, (O) this));
+            // Wurde eine neue Collection von n-Entities angegeben?
+            if (newCollection != null) {
+                // Alle neuen n-Entities hinzufügen
+                newCollection.forEach(e -> setEntity.accept(e, (O) this));
+            }
         }
 
         return collection;
@@ -85,34 +88,38 @@ public class Persistent {
      *      Datentyp der n-Seite
      * @param <C>
      *      {@link Collection} oder Subklasse
-     * @param newOne
+     * @param newEntity
      *      neu zuzuweisende Entity der 1-Seite
      * @param getCollection
      *      get-{@linkplain Function Methode} der 1-Seite für die {@link Collection} von Entities der n-Seite
-     * @param getOne
+     * @param getEntity
      *      get-{@linkplain Function Methode} der n-Seite für die Entity der 1-Seite
      * @return
      *      neu zugewiesene Entity der 1-Seite
      */
     protected <O extends Persistent, M extends Persistent, C extends Collection<M>>
-            O setManyToOne(O newOne, Function<O, C> getCollection, Function<M, O> getOne) {
+            O setManyToOne(O newEntity, Function<O, C> getCollection, Function<M, O> getEntity) {
 
-        O one;
-        C collection;
+        O entity = getEntity.apply((M) this);
 
-        // Gibt es derzeit eine 1-Entity, und gibt es dort eine Collection von n-Entities?
-        if (((one = getOne.apply((M) this)) != null) && ((collection = getCollection.apply(one)) != null)) {
-            // Diese Entity von der bisherigen 1-Entity entfernen
-            collection.remove(this);
+        // Soll überhaupt eine andere Entity zugewiesen werden?
+        if (newEntity != entity) {
+            C collection;
+
+            // Gibt es derzeit eine 1-Entity, und gibt es dort eine Collection von n-Entities?
+            if ((entity != null) && ((collection = getCollection.apply(entity)) != null)) {
+                // Diese Entity von der bisherigen 1-Entity entfernen
+                collection.remove(this);
+            }
+
+            // Wurde eine neue 1-Entity angegeben, und besitzt diese eine Collection von n-Entities?
+            if ((newEntity != null) && ((collection = getCollection.apply(newEntity)) != null)) {
+                // Diese Entity zur neuen 1-Entity hinzufügen
+                collection.add((M) this);
+            }
         }
 
-        // Wurde eine neue 1-Entity angegeben, und besitzt diese eine Collection von n-Entities?
-        if ((newOne != null) && ((collection = getCollection.apply(newOne)) != null)) {
-            // Diese Entity zur neuen 1-Entity hinzufügen
-            collection.add((M) this);
-        }
-
-        return newOne;
+        return newEntity;
     }
 
 }
